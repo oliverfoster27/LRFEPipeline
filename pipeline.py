9a0873a94bfee09c9444136ecff52c60659a5266
oliverfoster27 <oliverfoster27@gmail.com>
2019-10-24 19:56:41 -0400

    first pass of analysis

---
+++ pipeline.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import FeatureUnion, Pipeline
+
+
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    # Class Constructor
+    def __init__(self, feature_names):
+        self._feature_names = feature_names
+
+        # Return self nothing else to do here
+
+    def fit(self, X, y=None):
+        return self
+
+        # Method that describes what we need this transformer to do
+
+    def transform(self, X, y=None):
+        return X[self._feature_names]
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv(r"./data/housing_train.csv", index_col=0)
+    print(df.head())
